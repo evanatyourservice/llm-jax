da6de272b535690ce532e01576ff955737f95ad1
Evan Walters <evanatyourservice@gmail.com>
2024-08-30 15:55:20 -0600

    Update psgd_gpt.sh

--- scripts/psgd_gpt.sh
+++ scripts/psgd_gpt.sh
@@ -17,12 +17,12 @@ python3 train.py \
     --params_dtype=bfloat16 \
     --optimizer.type=affine \
     --optimizer.learning_rate=0.001 \
-    --optimizer.warmup_steps=512 \
-    --optimizer.no_nesterov \
+    --optimizer.warmup_steps=0 \
+    --optimizer.nesterov \
     --optimizer.weight_decay=0.01 \
     --optimizer.grad_clip=0.0 \
-    --optimizer.max_size_triangular=1000000000 \
-    --optimizer.max_skew_triangular=0 \
+    --optimizer.max_size_triangular=8192 \
+    --optimizer.max_skew_triangular=16 \
     --optimizer.precond_lr=0.1 \
-    --optimizer.precond_init_scale=1.0 \
+    --optimizer.precond_init_scale=0.1 \
     --optimizer.preconditioner_dtype=bfloat16
