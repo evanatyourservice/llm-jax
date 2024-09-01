67d4b59f6446f4b30d4024213a12e35c06dca8c7
Evan Walters <evanatyourservice@gmail.com>
2024-09-01 15:14:24 -0600

    Update psgd_gpt.sh

--- scripts/psgd_gpt.sh
+++ scripts/psgd_gpt.sh
@@ -8,7 +8,7 @@ export LLM_CONFIG=config/gpt2.yaml  # base config
 python3 train.py \
     --out_dir=gs://uscentral2stuff/llm-jax/$EXPERIMENT \
     --attempt_to_load_checkpoint \
-    --hellaswag_eval_interval=1000 \
+    --hellaswag_eval_interval=500 \
     --checkpoint_interval=1000 \
     --batch_size=256 \
     --optimizer.gradient_accumulation_steps=1 \
@@ -22,6 +22,6 @@ python3 train.py \
     --optimizer.grad_clip=1.0 \
     --optimizer.max_size_triangular=8192 \
     --optimizer.max_skew_triangular=16 \
-    --optimizer.precond_lr=0.1 \
+    --optimizer.precond_lr=0.01 \
     --optimizer.precond_init_scale=0.1 \
     --optimizer.preconditioner_dtype=bfloat16
