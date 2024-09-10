7f72d64849bb07de27ab37092c8d5e978bbe63e5
Evan Walters <evanatyourservice@gmail.com>
2024-09-10 11:59:48 -0600

    Update psgd_gpt_mh.sh

--- scripts/psgd_gpt_mh.sh
+++ scripts/psgd_gpt_mh.sh
@@ -21,7 +21,7 @@ python3 train.py \
     --attempt_to_load_checkpoint \
     --hellaswag_eval_interval=500 \
     --checkpoint_interval=1000 \
-    --train_steps=1000000 \
+    --train_steps=100000 \
     --batch_size=256 \
     --optimizer.gradient_accumulation_steps=1 \
     --compute_dtype=bfloat16 \
