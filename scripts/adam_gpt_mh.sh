8c1616f9c22ec2467b17332d73ca504d1e1b4ccd
Evan Walters <evanatyourservice@gmail.com>
2024-09-11 16:18:36 -0600

    scripts

--- scripts/adam_gpt_mh.sh
+++ scripts/adam_gpt_mh.sh
@@ -4,40 +4,36 @@
 WANDB_API_KEY=$1
 HF_TOKEN=$2
 
-gcloud compute tpus tpu-vm ssh --zone "us-central2-b" "LLaMA" --project "my-phd-research-o" \
---worker=all --command="
 EXPERIMENT=run_$(date +%Y-%m-%d_%H-%M-%S)
 echo $EXPERIMENT
 
+gcloud compute tpus tpu-vm ssh --zone "us-central2-b" "node-3" --project "distributedmuzerojax" \
+--worker=all --command "bash -c \"
 export WANDB_API_KEY=$WANDB_API_KEY
 export HF_TOKEN=$HF_TOKEN
-
 export LLM_CONFIG=config/gpt2.yaml
-
 cd llm-jax
-
-python3 train.py \
-    --out_dir=gs://uscentral2stuff/llm-jax/$EXPERIMENT \
+nohup python3 main_multihost.py \
+    --experiment_name=$EXPERIMENT \
+    --out_dir=gs://optimizertesting/llm-jax \
     --attempt_to_load_checkpoint \
-    --hellaswag_eval_interval=500 \
+    --hellaswag_eval_interval=1000 \
     --checkpoint_interval=1000 \
-    --train_steps=100000 \
-    --batch_size=1024 \
-    --optimizer.gradient_accumulation_steps=1 \
-    --compute_dtype=bfloat16 \
+    --train_steps=200000 \
+    --batch_size=256 \
+    --optimizer.gradient_accumulation_steps=2 \
+    --compute_dtype=float32 \
     --params_dtype=float32 \
-    --optimizer.type=affine \
+    --optimizer.type=adamw \
     --optimizer.learning_rate=0.003 \
-    --optimizer.warmup_steps=0 \
+    --optimizer.warmup_steps=1000 \
     --optimizer.weight_decay=0.01 \
     --optimizer.grad_clip=1.0 \
-    --optimizer.max_size_triangular=16384 \
+    --optimizer.max_size_triangular=8192 \
     --optimizer.max_skew_triangular=16 \
     --optimizer.precond_lr=0.1 \
     --optimizer.precond_init_scale=1.0 \
-    --optimizer.preconditioner_dtype=float32 \
-    --model.block_size=1024 \
-    --model.num_layers=24 \
-    --model.num_heads=16 \
-    --model.num_embeds=1024
-"
+    --optimizer.preconditioner_dtype=float32 > nohup.out 2>&1 &
+echo 'Background process started'
+exit
+\""
--- scripts/psgd_gpt_mh64.sh
+++ scripts/psgd_gpt_mh64.sh
@@ -7,7 +7,7 @@ HF_TOKEN=$2
 EXPERIMENT=run_$(date +%Y-%m-%d_%H-%M-%S)
 echo $EXPERIMENT
 
-gcloud compute tpus tpu-vm ssh --zone "us-central2-b" "node-3" --project "distributedmuzerojax" \
+gcloud compute tpus tpu-vm ssh --zone "us-central2-b" "LLaMA" --project "my-phd-research-o" \
 --worker=all --command "bash -c \"
 export WANDB_API_KEY=$WANDB_API_KEY
 export HF_TOKEN=$HF_TOKEN
@@ -19,18 +19,21 @@ nohup python3 main_multihost.py \
     --attempt_to_load_checkpoint \
     --hellaswag_eval_interval=1000 \
     --checkpoint_interval=1000 \
-    --train_steps=200000 \
-    --batch_size=256 \
-    --optimizer.gradient_accumulation_steps=2 \
+    --train_steps=191000 \
+    --batch_size=128 \
+    --optimizer.gradient_accumulation_steps=4 \
     --compute_dtype=float32 \
     --params_dtype=float32 \
+    --model.num_layers=24 \
+    --model.num_heads=24 \
+    --model.num_embeds=2304 \
     --optimizer.type=affine \
     --optimizer.learning_rate=0.003 \
     --optimizer.warmup_steps=0 \
     --optimizer.weight_decay=0.01 \
     --optimizer.grad_clip=1.0 \
-    --optimizer.max_size_triangular=100000 \
-    --optimizer.max_skew_triangular=0 \
+    --optimizer.max_size_triangular=10000 \
+    --optimizer.max_skew_triangular=16 \
     --optimizer.precond_lr=0.1 \
     --optimizer.precond_init_scale=0.1 \
     --optimizer.preconditioner_dtype=float32 > nohup.out 2>&1 &
