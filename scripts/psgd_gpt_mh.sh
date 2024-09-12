f3de64042736bae2d2df86fc70a88d51955d16bb
Evan Walters <evanatyourservice@gmail.com>
2024-09-12 11:46:24 -0600

    Update psgd_gpt_mh.sh

--- scripts/psgd_gpt_mh.sh
+++ scripts/psgd_gpt_mh.sh
@@ -7,12 +7,12 @@ HF_TOKEN=$2
 EXPERIMENT=run_$(date +%Y-%m-%d_%H-%M-%S)
 echo $EXPERIMENT
 
-gcloud compute tpus tpu-vm ssh --zone "us-central2-b" "node-3" --project "distributedmuzerojax" \
---worker=all --command "bash -c \"
-export WANDB_API_KEY=$WANDB_API_KEY
-export HF_TOKEN=$HF_TOKEN
-export LLM_CONFIG=config/gpt2.yaml
-cd llm-jax
+gcloud compute tpus tpu-vm ssh --zone "us-central2-b" "node-2" --project "distributedmuzerojax" \
+--worker=all --command="
+export WANDB_API_KEY=$WANDB_API_KEY && \
+export HF_TOKEN=$HF_TOKEN && \
+export LLM_CONFIG=config/gpt2.yaml && \
+cd llm-jax && \
 nohup python3 main_multihost.py \
     --experiment_name=$EXPERIMENT \
     --out_dir=gs://optimizertesting/llm-jax \
@@ -25,7 +25,7 @@ nohup python3 main_multihost.py \
     --compute_dtype=bfloat16 \
     --params_dtype=float32 \
     --optimizer.type=affine \
-    --optimizer.learning_rate=0.003 \
+    --optimizer.learning_rate=0.01 \
     --optimizer.warmup_steps=500 \
     --optimizer.weight_decay=0.1 \
     --optimizer.grad_clip=1.0 \
@@ -34,7 +34,5 @@ nohup python3 main_multihost.py \
     --optimizer.max_skew_triangular=10 \
     --optimizer.precond_lr=0.2 \
     --optimizer.precond_init_scale=1.0 \
-    --optimizer.preconditioner_dtype=float32 > nohup.out 2>&1 &
-echo 'Background process started'
-exit
-\""
+    --optimizer.preconditioner_dtype=float32
+"
