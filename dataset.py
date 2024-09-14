fix hellaswag masking, retouches

dataset.py
@@ -49,41 +49,57 @@ def prepare_hellaswag(
     )
 
     all_data = []
-    all_seq_lens = []
+    all_beginning_lengths = []
+    all_seq_lengths = []
     all_labels = []
     with open("data/hellaswag_val.jsonl", "r") as f:
         # iterate over lines and tokenize
         for line in tqdm(f, total=10042):
             item = json.loads(line)
+
             context = item["ctx"]
             endings = item["endings"]
             correct_end = item["label"]
+
+            beginning_length = len(tokenizer(context)["input_ids"])
+
             data_to_concat = []
-            seq_lens_to_concat = []
+            beginning_lengths_to_concat = []
+            seq_lengths_to_concat = []
             for ending in endings:
-                input_text = context + " " + ending
-                # encode with tokenizer
-                output = tokenizer(input_text)["input_ids"]
-                # output len, at least block_size
-                output_len = min(len(output), block_size)
-                # pad if less than block_size
-                if output_len < block_size:
+                output = tokenizer(context + " " + ending)["input_ids"]
+
+                # pad to block_size
+                if len(output) < block_size:
                     output = output + [tokenizer.eos_token_id] * (
-                        block_size - output_len
+                        block_size - len(output)
                     )
                 # max length is block_size
                 output = output[:block_size]
+
                 data_to_concat.append(output)
-                seq_lens_to_concat.append(output_len)
+                beginning_lengths_to_concat.append(beginning_length)
+                seq_lengths_to_concat.append(len(output))
+
             all_data.append(np.array(data_to_concat, dtype=np.uint16))  # (4, seq_len)
-            all_seq_lens.append(np.array(seq_lens_to_concat, dtype=np.int32))  # (4,)
+            all_beginning_lengths.append(
+                np.array(beginning_lengths_to_concat, dtype=np.int32)
+            )  # (4,)
+            all_seq_lengths.append(
+                np.array(seq_lengths_to_concat, dtype=np.int32)
+            )  # (4,)
             all_labels.append(int(correct_end))  # []
 
     all_data = np.array(all_data, dtype=np.uint16)  # (10042, 4, seq_len)
-    all_seq_lens = np.array(all_seq_lens, dtype=np.int32)  # (10042, 4)
+    all_beginning_lengths = np.array(
+        all_beginning_lengths, dtype=np.int32
+    )  # (10042, 4)
+    all_seq_lengths = np.array(all_seq_lengths, dtype=np.int32)  # (10042, 4)
     all_labels = np.array(all_labels, dtype=np.int32)  # (10042,)
 
-    ds = tf.data.Dataset.from_tensor_slices((all_data, all_seq_lens, all_labels))
+    ds = tf.data.Dataset.from_tensor_slices(
+        (all_data, all_beginning_lengths, all_seq_lengths, all_labels)
+    )
     ds = ds.shard(jax.process_count(), jax.process_index())
     ds = ds.repeat()
 
@@ -137,6 +153,7 @@ def fineweb_edu_dataset(
         )
 
         def tokenize(example):
+            # mistral tokenizer adds bos token to beginning
             tokenized = tokenizer(example)["input_ids"]
             # cap tokenized lengths to 10 * block_size and add eot_token
             tokenized = [t[: 10 * block_size] for t in tokenized]

model/mistral.py
@@ -100,7 +100,9 @@ class Block(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        attn_layer = Attention(self.num_heads, self.num_kv_heads, self.head_dim, self.rope_theta)
+        attn_layer = Attention(
+            self.num_heads, self.num_kv_heads, self.head_dim, self.rope_theta
+        )
 
         attn_mask = nn.make_causal_mask(x[:1, :, 0], dtype=jnp.bool)
 
@@ -131,14 +133,18 @@ class Mistral(nn.Module):
         x *= jnp.sqrt(self.config.num_embeds).astype(x.dtype)
 
         if self.config.scan_layers:
-            x = flax_scan(Block, self.config.num_layers, unroll=self.config.scan_unroll)(
+            x = flax_scan(
+                Block, self.config.num_layers, unroll=self.config.scan_unroll
+            )(
                 self.config.num_heads,
                 self.config.num_kv_heads,
                 self.config.head_dim,
                 self.config.sliding_window_size,
                 self.config.hidden_dim,
                 self.config.rope_theta,
-            )(x)
+            )(
+                x
+            )
         else:
             for _ in range(self.config.num_layers):
                 x = Block(

train.py
@@ -101,18 +101,23 @@ def main(config: TrainConfig):
 
     # ====== optimizer ======
     write_note("Creating optimizer")
+    # lr schedule keeps flat for 10k steps after warmup, then linear decay
     lr_schedule = optax.join_schedules(
         schedules=[
             optax.linear_schedule(
                 0.0, config.optimizer.learning_rate, config.optimizer.warmup_steps
             ),
+            optax.constant_schedule(config.optimizer.learning_rate),
             optax.linear_schedule(
                 config.optimizer.learning_rate,
                 config.optimizer.learning_rate * 0.05,
-                config.train_steps - config.optimizer.warmup_steps,
+                config.train_steps - config.optimizer.warmup_steps - 10000,
             ),
         ],
-        boundaries=[config.optimizer.warmup_steps],
+        boundaries=[
+            config.optimizer.warmup_steps,
+            config.optimizer.warmup_steps + 10000,
+        ],
     )
 
     def make_opt(reshaped_params_sharding=None):
@@ -370,9 +375,11 @@ def main(config: TrainConfig):
 
             targets = tokens[:, 1:]
 
-            loss = optax.softmax_cross_entropy_with_integer_labels(
-                logits, targets
-            ).mean()
+            loss = optax.softmax_cross_entropy_with_integer_labels(logits, targets)
+
+            bos_token_id = 1
+            mask = targets != bos_token_id
+            loss = jnp.sum(loss * mask) / jnp.sum(mask)
 
             return loss
 
@@ -402,8 +409,11 @@ def main(config: TrainConfig):
 
         return loss, new_state, grad_norm, lr
 
-    def eval_step_unreduced(
-        state: TrainState, tokens: jnp.ndarray, seq_lens: jnp.ndarray
+    def hs_eval_step_unreduced(
+        state: TrainState,
+        tokens: jnp.ndarray,
+        begin_lens: jnp.ndarray,
+        seq_lens: jnp.ndarray,
     ) -> jnp.ndarray:
         logits = state.apply_fn(
             otu.tree_cast(state.params, config.compute_dtype), tokens[:, :-1]
@@ -417,24 +427,27 @@ def main(config: TrainConfig):
         losses = optax.softmax_cross_entropy_with_integer_labels(logits, targets)
 
         @jax.vmap
-        def unreduced_losses(loss, seq_len):
-            seq_mask = jnp.arange(len(loss)) < seq_len - 1
-            seq_mask = seq_mask.astype(logits.dtype)
+        def unreduced_losses(loss, begin_len, seq_len):
+            seq_range = jnp.arange(len(loss))
+            seq_mask = jnp.logical_and(
+                seq_range < seq_len - 1, seq_range >= begin_len - 1
+            ).astype(jnp.bool)
             loss = loss * seq_mask
             return jnp.sum(loss) / jnp.sum(seq_mask)
 
-        losses = unreduced_losses(losses, seq_lens)
+        losses = unreduced_losses(losses, begin_lens, seq_lens)
         return losses
 
-    def eval_hellaswag(state: TrainState, data, seq_lens, labels):
+    def eval_hellaswag(state: TrainState, data, begin_lens, seq_lens, labels):
         """Evaluate the hellaswag dataset."""
         # data comes in shape (b, 4, block_size + 1)
-        # masks comes in shape (b, 4, block_size + 1)
-        # labels comes in shape (b,)
+        # seq lens come in shape (b, 4)
+        # labels come in shape (b,)
         bs_in = data.shape[0]
         data = jnp.reshape(data, (-1, data.shape[-1]))
+        begin_lens = jnp.reshape(begin_lens, (-1,))
         seq_lens = jnp.reshape(seq_lens, (-1,))
-        losses = eval_step_unreduced(state, data, seq_lens)
+        losses = hs_eval_step_unreduced(state, data, begin_lens, seq_lens)
         choices = jnp.argmin(jnp.reshape(losses, (bs_in, 4)), axis=-1)
         correct = jnp.sum(choices == labels)
         accuracy = correct / bs_in
@@ -457,6 +470,7 @@ def main(config: TrainConfig):
         eval_hellaswag,
         in_shardings=(
             train_state_sharding,
+            data_sharding,
             data_sharding,
             data_sharding,
             data_sharding,
@@ -561,7 +575,10 @@ def main(config: TrainConfig):
                 )
 
                 # eval hellaswag
-                if curr_step % config.hellaswag_eval_interval == 0:
+                if (
+                    curr_step % config.hellaswag_eval_interval == 0
+                    and config.model.block_size >= 1024
+                ):
                     hs_accs = []
                     for _ in range(10 if platform == "cpu" else 10042 // hs_batch_size):
                         hs_batch = next(hellaswag_ds)