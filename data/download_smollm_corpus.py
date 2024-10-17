7456bfca96b831b39f69c1c46c464bbd423d4a7b
Evan Walters <evanatyourservice@gmail.com>
2024-10-16 21:50:33 -0600

    Update download_smollm_corpus.py

--- data/download_smollm_corpus.py
+++ data/download_smollm_corpus.py
@@ -9,7 +9,7 @@ fs = gcsfs.GCSFileSystem(**storage_options)
 # fineweb-edu-dedup
 print("Downloading fineweb-edu-dedup")
 output_dir = "gs://optimizertesting/smollm-corpus/fineweb-edu-dedup"
-builder = load_dataset_builder("HuggingFaceTB/smollm-corpus", "fineweb-edu-dedup")
+builder = load_dataset_builder("HuggingFaceTB/smollm-corpus", "fineweb-edu-dedup", cache_dir="/hf")
 builder.download_and_prepare(
     output_dir=output_dir, storage_options=storage_options, file_format="parquet"
 )
@@ -17,7 +17,7 @@ builder.download_and_prepare(
 # cosmopedia-v2
 print("Downloading cosmopedia-v2")
 output_dir = "gs://optimizertesting/smollm-corpus/cosmopedia-v2"
-builder = load_dataset_builder("HuggingFaceTB/smollm-corpus", "cosmopedia-v2")
+builder = load_dataset_builder("HuggingFaceTB/smollm-corpus", "cosmopedia-v2", cache_dir="/hf")
 builder.download_and_prepare(
     output_dir=output_dir, storage_options=storage_options, file_format="parquet"
 )
