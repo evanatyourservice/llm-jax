37fd493434201c7be7f60f76356a1f481b7d9ca1
Evan Walters <evanatyourservice@gmail.com>
2024-10-17 23:21:20 -0600

    Update kron.py

--- optimizers/kron.py
+++ optimizers/kron.py
@@ -84,7 +84,7 @@ def scale_by_kron(
 
     # some hardcoded settings
     precond_lr = 0.1
-    precond_init_scale = 0.1
+    precond_init_scale = 1.0
     momentum_into_preconditioner = True
 
     def map_fn(do_map, fn, *args):
